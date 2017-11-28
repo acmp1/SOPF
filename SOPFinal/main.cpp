a222b6f722d44ff7c563708de3f369f357dca084
Clarissa Miranda <clarissamiranda@MacBook-Pro-de-Clarissa.local>
2017-11-27 18:31:15 -0600

    Adds p procedure

--- SOPFinal/main.cpp
+++ SOPFinal/main.cpp
@@ -6,19 +6,75 @@
 //  Copyright © 2017 Clarissa Miranda. All rights reserved.
 //
 
-
 #include <iostream>
 #include <fstream>
+#include <cmath>
+#include <queue>
 
 using namespace std;
 
-void iniciarArchivo(ifstream archivoLeer,ostream output)
+int swa[4096]={0};
+int mp[127]={0};
+int m[2048]={0};
+int iC=0;
+int iLibre=127;
+queue<pair<int,int>> fifoP;
+
+void imprimirArreglo()
 {
+    for(int i=0;mp[i]!=0;i++)
+    {
+        cout << mp[i] << endl;
+    }
+}
+
+void swapFifo(int iN, int iP)
+{
+    iC = ceil(iN/16.0);
+    for(int i=0;i<iC;i++)
+    {
+        if((swa[i]==0)&&iC>0)
+        {
+            fifoP.front();
+            swa[i]=iP;
+            iC--;
+        }
+    }
+    iLibre -= iC;
+}
+
+// Función que coloca un proceso en memoria ejecutable
+void colocarProceso(int iN, int iP)
+{
+    iC = ceil(iN/16.0);
+    if(iLibre >= iC)
+    {
+        for(int i=0;i<128;i++)
+        {
+            if(mp[i]==0&&iC>0)
+            {
+                mp[i]=iP;
+                fifoP.push(make_pair(iP,i));
+                iC--;
+            }
+        }
+        iLibre -= iC;
+    }
+    else
+    {
+        swapFifo(iN,iP);
+    }
+}
+
+void iniciarArchivo()
+{
+    ifstream archivoLeer;
     char cOpc;
     int iD,iN,iP,iM;
     string sReplica;
     
-    archivoLeer.open("VirtualMemoryTestFileFifoLru.txt");
+    archivoLeer.open("prueba.txt");
+    
     while(archivoLeer>>cOpc)
     {
         if(cOpc=='C')
@@ -29,6 +85,8 @@ void iniciarArchivo(ifstream archivoLeer,ostream output)
         if(cOpc=='P')
         {
             archivoLeer >> iN >> iP;
+            colocarProceso(iN, iP);
+            
         }
         if(cOpc=='A')
         {
@@ -40,15 +98,19 @@ void iniciarArchivo(ifstream archivoLeer,ostream output)
         }
         if(cOpc=='F')
         {
-            output << endl;
+            
+        }
+        if(cOpc=='E')
+        {
+            imprimirArreglo();
         }
     }
     
 }
 
 int main() {
-    
-    ifstream archivoLeer;
+
+    iniciarArchivo();
     
     return 0;
 }
